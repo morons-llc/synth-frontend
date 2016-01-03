9ed3725ce4d09f3c3dac0ad0a423f8e770e5affa
David Balatero <dbalatero@gmail.com>
2016-01-02 22:34:00 -0800

    Silence warning

--- src/js/entry.js
+++ src/js/entry.js
@@ -5,5 +5,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Render!
-console.log("TEST");
-ReactDOM.render(<HelloBox />, document.getElementsByTagName('body')[0]);
+ReactDOM.render(<HelloBox />, document.getElementById('app-container'));
