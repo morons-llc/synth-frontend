5060f59b3bf5859b5559f05681de53daf1e6a4a9
bion <bion@citizencode.io>
2016-01-11 01:08:58 -0800

    Use local props instead of passing arg

--- src/js/containers/patch_app.js
+++ src/js/containers/patch_app.js
@@ -3,65 +3,65 @@ import MIDIRangeSelector from '../components/midi_range_selector'
 import { connect } from 'react-redux'
 
 const PatchApp = (props) => {
+  function setParam(path) {
+    return (event) => {
+      props.dispatch({
+        type: 'SET_PARAM',
+        path: path,
+        value: parseInt(event.target.value, 10)
+      })
+    }
+  }
+
+  function rangeSelectorFor(path, name) {
+    return (
+      <MIDIRangeSelector
+        name={ name }
+        path={ path }
+        value={ props.patchState.getIn(path.split('.')) }
+        onChange={ setParam(path) }
+      />
+    )
+  }
+
   return (
     <div className="synth-control">
       <section className="oscillator">
         <h2>DCO</h2>
-        { rangeSelectorFor('osc.osc1.lfo', 'LFO', props) }
-        { rangeSelectorFor('osc.osc1.pwmLevel', 'PWM', props) }
-        { rangeSelectorFor('osc.osc1.subOsc', 'Sub', props) }
-        { rangeSelectorFor('osc.osc1.noise', 'Noise', props) }
+        { rangeSelectorFor('osc.osc1.lfo', 'LFO') }
+        { rangeSelectorFor('osc.osc1.pwmLevel', 'PWM') }
+        { rangeSelectorFor('osc.osc1.subOsc', 'Sub') }
+        { rangeSelectorFor('osc.osc1.noise', 'Noise') }
       </section>
       <section className="mod">
         <h2>LFO</h2>
-        { rangeSelectorFor('mod.lfo.rate', 'Rate', props) }
-        { rangeSelectorFor('mod.lfo.delay', 'Delay', props) }
+        { rangeSelectorFor('mod.lfo.rate', 'Rate') }
+        { rangeSelectorFor('mod.lfo.delay', 'Delay') }
       </section>
       <section className="filter">
         <h2>VCF</h2>
-        { rangeSelectorFor('filter.frequency', 'Freq', props) }
-        { rangeSelectorFor('filter.resonance', 'Res', props) }
-        { rangeSelectorFor('filter.envelopeAmount', 'Env', props) }
-        { rangeSelectorFor('filter.lfo', 'LFO', props) }
-        { rangeSelectorFor('filter.keyboardTracking', 'KYBD', props) }
+        { rangeSelectorFor('filter.frequency', 'Freq') }
+        { rangeSelectorFor('filter.resonance', 'Res') }
+        { rangeSelectorFor('filter.envelopeAmount', 'Env') }
+        { rangeSelectorFor('filter.lfo', 'LFO') }
+        { rangeSelectorFor('filter.keyboardTracking', 'KYBD') }
       </section>
       <section className="HPF">
         <h2>HPF</h2>
-        { rangeSelectorFor('filter.hpf', 'Freq', props) }
+        { rangeSelectorFor('filter.hpf', 'Freq') }
       </section>
       <section className="envelope">
         <h2>ENV</h2>
-        { rangeSelectorFor('envelope.attack', 'A', props) }
-        { rangeSelectorFor('envelope.decay', 'D', props) }
-        { rangeSelectorFor('envelope.sustain', 'S', props) }
-        { rangeSelectorFor('envelope.release', 'R', props) }
+        { rangeSelectorFor('envelope.attack', 'A') }
+        { rangeSelectorFor('envelope.decay', 'D') }
+        { rangeSelectorFor('envelope.sustain', 'S') }
+        { rangeSelectorFor('envelope.release', 'R') }
       </section>
       <section className="vca">
         <h2>VCA</h2>
-        { rangeSelectorFor('amp.level', 'Level', props) }
+        { rangeSelectorFor('amp.level', 'Level') }
       </section>
     </div>
-  )
-}
-
-function setParam(path, dispatch) {
-  return (event) => {
-    dispatch({
-      type: 'SET_PARAM',
-      path: path,
-      value: parseInt(event.target.value, 10)
-    })
-  }
-}
-
-function rangeSelectorFor(path, name, props) {
-  return (
-    <MIDIRangeSelector
-    name={ name }
-    path={ path }
-    value={ props.patchState.getIn(path.split('.')) }
-    onChange={ setParam(path, props.dispatch) }
-    />
   )
 }
 
