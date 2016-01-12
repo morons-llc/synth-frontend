36491cce94e7d59929a0c42a92009a4a4d2cef9f
bion <bion@citizencode.io>
2016-01-12 00:04:09 -0800

    Add clamping option for ParamSpec

--- src/js/util/param_spec.js
+++ src/js/util/param_spec.js
@@ -1,13 +1,37 @@
-function ParamSpec(sourceLow, sourceHigh, distLow, distHigh) {
+function ParamSpec(sourceLow, sourceHigh, distLow, distHigh, clamp = true) {
   const domain = sourceHigh - sourceLow
   const range = distHigh - distLow
+  let map
+  let unmap
 
-  function map(val) {
-    return (((val - sourceLow) / domain) * range) + distLow
-  }
+  if (clamp) {
+    map = (val) => {
+      let result
 
-  function unmap(val) {
-    return (((val - distLow) / range) * domain) + sourceLow
+      if (val < sourceLow) result = distLow
+      else if (val > sourceHigh) result = distHigh
+      else result = (((val - sourceLow) / domain) * range) + distLow
+
+      return result
+    }
+
+    unmap = (val) => {
+      let result
+
+      if (val < distLow) result = sourceLow
+      else if (val > distHigh) result = sourceHigh
+      else result = (((val - distLow) / range) * domain) + sourceLow
+
+      return result
+    }
+  } else {
+    map = (val) => {
+      return (((val - sourceLow) / domain) * range) + distLow
+    }
+
+    unmap = (val) => {
+      return (((val - distLow) / range) * domain) + sourceLow
+    }
   }
 
   return { map, unmap }
--- test/util/param_spec_test.js
+++ test/util/param_spec_test.js
@@ -1,35 +1,79 @@
 import ParamSpec from "../../src/js/util/param_spec"
 
 describe('ParamSpec', function () {
-  beforeEach(function () {
-    this.subject = ParamSpec(0, 127, 0, 10)
+  context('with clamping', function () {
+    beforeEach(function () {
+      this.subject = ParamSpec(0, 127, 0, 10)
+    })
+
+    describe('map', function () {
+      it('maps a value to the distribution', function () {
+        expect(this.subject.map(63.5)).to.eql(5)
+      })
+
+      it('maps a value to the distribution', function () {
+        expect(this.subject.map(0)).to.eql(0)
+      })
+
+      it('maps a value to the distribution', function () {
+        expect(this.subject.map(127)).to.eql(10)
+      })
+
+      it('maps a value above domain to the distribution max', function () {
+        expect(this.subject.map(200)).to.eql(10)
+      })
+
+      it('maps a value below domain to the distribution min', function () {
+        expect(this.subject.map(-20)).to.eql(0)
+      })
+    })
+
+    describe('unmap', function () {
+      it('unmaps a value from the distribution', function () {
+        expect(this.subject.unmap(5)).to.eql(63.5)
+      })
+
+      it('unmaps a value from the distribution', function () {
+        expect(this.subject.unmap(0)).to.eql(0)
+      })
+
+      it('unmaps a value from the distribution', function () {
+        expect(this.subject.unmap(10)).to.eql(127)
+      })
+
+      it('unmaps a value above range to the source max', function () {
+        expect(this.subject.unmap(20)).to.eql(127)
+      })
+
+      it('unmaps a value below range to the source min', function () {
+        expect(this.subject.unmap(-20)).to.eql(0)
+      })
+    })
   })
 
-  describe('map', function () {
-    it('maps a value to the distribution', function () {
-      expect(this.subject.map(63.5)).to.eql(5)
+  context('without clamping', function () {
+    beforeEach(function () {
+      this.subject = ParamSpec(0, 127, 0, 10, false)
     })
 
-    it('maps a value to the distribution', function () {
-      expect(this.subject.map(0)).to.eql(0)
+    describe('map', function () {
+      it('maps a value above domain to an extrapolation', function () {
+        expect(this.subject.map(190.5)).to.eql(15)
+      })
+
+      it('maps a value below domain to an exdrapolation', function () {
+        expect(this.subject.map(-52.07)).to.eql(-4.1)
+      })
     })
 
-    it('maps a value to the distribution', function () {
-      expect(this.subject.map(127)).to.eql(10)
-    })
-  })
+    describe('unmap', function () {
+      it('unmaps a value above range to an extrapolation', function () {
+        expect(this.subject.unmap(15)).to.eql(190.5)
+      })
 
-  describe('unmap', function () {
-    it('unmaps a value from the distribution', function () {
-      expect(this.subject.unmap(5)).to.eql(63.5)
-    })
-
-    it('unmaps a value from the distribution', function () {
-      expect(this.subject.unmap(0)).to.eql(0)
-    })
-
-    it('unmaps a value from the distribution', function () {
-      expect(this.subject.unmap(10)).to.eql(127)
+      it('unmaps a value below range to an extrapolation', function () {
+        expect(this.subject.unmap(-4.1)).to.eql(-52.07)
+      })
     })
   })
 })
