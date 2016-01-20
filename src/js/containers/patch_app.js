dbadefa7784b679142bc9487437e78440df778ac
bion <bion@citizencode.io>
2016-01-20 10:53:48 -0800

    Add chorus and waveform toggles

--- src/js/containers/patch_app.js
+++ src/js/containers/patch_app.js
@@ -2,22 +2,35 @@ import React, { PropTypes } from 'react'
 import RangeMapper from '../util/range_mapper'
 import RangeSelector from '../components/range_selector'
 import { connect } from 'react-redux'
+import OptionsSelector from '../components/options_selector'
 
 const rangeMapper = RangeMapper(0, 127, 0, 10)
 
+function extractRangeValue(event) {
+  return rangeMapper.unmap(parseInt(event.target.value, 10))
+}
+
+function extractBooleanValue(event) {
+  return event.target.value === 'true'
+}
+
 const PatchApp = (props) => {
-  function setParam(path) {
+  function valueAt(path) {
+    return props.patchState.getIn(path.split('.'))
+  }
+
+  function setParam(path, valueExtractor) {
     return (event) => {
       props.dispatch({
         type: 'SET_PARAM',
         path: path,
-        value: rangeMapper.unmap(parseInt(event.target.value, 10))
+        value: valueExtractor(event)
       })
     }
   }
 
   function rangeSelectorFor(path, name) {
-    const value = props.patchState.getIn(path.split('.'))
+    const value = valueAt(path)
     const displayValue = rangeMapper.map(value)
 
     return (
@@ -25,7 +38,72 @@ const PatchApp = (props) => {
         name={ name }
         path={ path }
         value={ displayValue }
-        onChange={ setParam(path) }
+        onChange={ setParam(path, extractRangeValue) }
+      />
+    )
+  }
+
+  function booleanSelectorFor(path, name, trueText = 'on', falseText = 'off') {
+    const boolVal = valueAt(path)
+    const options = [
+      {
+        optionName: trueText,
+        value: true,
+        checked: boolVal
+      },
+      {
+        optionName: falseText,
+        value: false,
+        checked: !boolVal
+      }
+    ]
+
+    return (
+      <OptionsSelector
+        name={ name }
+        onChange={ setParam(path, extractBooleanValue) }
+        options={ options }
+      />
+    )
+  }
+
+  function juno106WaveFormSelector() {
+    const pulsePath = 'osc.osc1.pulseWave'
+    const triPath = 'osc.osc1.triangleWave'
+    const options = [
+      {
+        optionName: 'Pulse Wave',
+        value: pulsePath,
+        checked: valueAt(pulsePath)
+      },
+      {
+        optionName: 'Triangle Wave',
+        value: triPath,
+        checked: valueAt(triPath)
+      }
+    ]
+
+    const update = (event) => {
+      props.dispatch({
+        type: 'SET_PARAMS',
+        params: [
+          {
+            path: pulsePath,
+            value: event.target.value === pulsePath
+          },
+          {
+            path: triPath,
+            value: event.target.value === triPath
+          }
+        ]
+      })
+    }
+
+    return (
+      <OptionsSelector
+        name="Wave Form"
+        onChange={ update }
+        options={ options }
       />
     )
   }
@@ -38,7 +116,8 @@ const PatchApp = (props) => {
         { rangeSelectorFor('osc.osc1.pwmLevel', 'PWM') }
         { rangeSelectorFor('osc.osc1.subOsc', 'Sub') }
         { rangeSelectorFor('osc.osc1.noise', 'Noise') }
-      </section>
+        { juno106WaveFormSelector() }
+    </section>
       <section className="mod">
         <h2>LFO</h2>
         { rangeSelectorFor('mod.lfo.rate', 'Rate') }
@@ -66,6 +145,10 @@ const PatchApp = (props) => {
       <section className="vca">
         <h2>VCA</h2>
         { rangeSelectorFor('amp.level', 'Level') }
+      </section>
+      <section className="chorus">
+        <h2>Chorus</h2>
+        { booleanSelectorFor('chorus.disabled', '', 'disabled', 'enabled') }
       </section>
     </div>
   )
